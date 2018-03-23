1ba5a2f0fb032440f028c559150bfdb1a04a8d25
phlaffez <phlaffez2@gmail.com>
2018-03-23 10:12:55 +0100

    Ajout de la classe MainTest pour pouvoir travailler. Elle dispose d'une fonction Main

---
+++ src/phl/outils/panneaux/outilsStandards/MainTest.java
@@ -0,0 +1,12 @@
+package phl.outils.panneaux.outilsStandards;
+
+public class MainTest {
+
+	public static void main(String[] args)
+	{
+		FenetreTest pos = new FenetreTest();
+		System.out.println("terminé");
+
+	}
+
+}
