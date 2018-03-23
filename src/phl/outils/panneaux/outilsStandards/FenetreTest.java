666d33155526581dffa7ba50a041aa86c1f33e29
phlaffez <phlaffez2@gmail.com>
2018-03-23 10:08:29 +0100

    Ajout d'une classe FenetreTest pour pouvoir tester les composants fabriqués ici. Juste le squelette. Doit être programmée

---
+++ src/phl/outils/panneaux/outilsStandards/FenetreTest.java
@@ -0,0 +1,13 @@
+package phl.outils.panneaux.outilsStandards;
+
+import javax.swing.JPanel;
+
+public class FenetreTest extends JPanel
+{
+
+	public FenetreTest()
+	{
+	this.setVisible(true);
+	}
+	
+}
